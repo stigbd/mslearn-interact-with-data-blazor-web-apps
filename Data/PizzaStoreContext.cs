ef49f8ff782499979df50feaa70982f8c121fb72
Stig B. Dørmænen <stigbd@gmail.com>
2022-11-01 10:44:30 +0100

    chore: my first commit

---
+++ Data/PizzaStoreContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Data;
+
+public class PizzaStoreContext : DbContext
+{
+    public PizzaStoreContext(DbContextOptions<PizzaStoreContext> options) : base(options)
+    {
+    }
+
+    public DbSet<PizzaSpecial> PizzaSpecials { get; set; }
+}
